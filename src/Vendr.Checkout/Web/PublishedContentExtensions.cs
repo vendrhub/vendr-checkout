132b98046a7a6b54ae7bcf93fd61975ca09fcb89
mattbrailsford <me@mattbrailsford.com>
2020-02-11 11:02:15 +0000

    Copying over some elements from demo store

--- src/Vendr.Checkout/Web/PublishedContentExtensions.cs
+++ src/Vendr.Checkout/Web/PublishedContentExtensions.cs
@@ -1,12 +1,25 @@
 ﻿using Umbraco.Web;
 using Umbraco.Core.Models.PublishedContent;
-using Vendr.Core;
 using Vendr.Core.Models;
+using Umbraco.Core;
+
+using Constants = Vendr.Core.Constants;
 
 namespace Vendr.Checkout.Web
 {
     public static class PublishedContentExtensions
     {
+        public static string GetMetaTitle(this IPublishedContent content)
+        {
+            var storeName = content.Value<string>("vendrStoreName", fallback:Fallback.ToAncestors);
+            var pageTitle = content.Value<string>("pageTitle");
+
+            if (!pageTitle.IsNullOrWhiteSpace())
+                return $"{pageTitle} | {storeName}";
+
+            return $"{content.Name} | {storeName}";
+        }
+
         public static StoreReadOnly GetStore(this IPublishedContent content)
         {
             return content.Value<StoreReadOnly>(Constants.Properties.StorePropertyAlias, fallback:Fallback.ToAncestors);
