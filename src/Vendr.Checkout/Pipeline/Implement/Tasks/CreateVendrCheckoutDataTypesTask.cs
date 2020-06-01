63840beffc80c5502ad1f95198d67ab7eb3648bf
mattbrailsford <me@mattbrailsford.com>
2020-06-01 15:05:44 +0100

    Added code to replace data type pre-values if the data types already exist

--- src/Vendr.Checkout/Pipeline/Implement/Tasks/CreateVendrCheckoutDataTypesTask.cs
+++ src/Vendr.Checkout/Pipeline/Implement/Tasks/CreateVendrCheckoutDataTypesTask.cs
@@ -12,7 +12,8 @@ namespace Vendr.Checkout.Pipeline.Implement.Tasks
         public InstallPipelineContext Process(InstallPipelineContext ctx)
         {
             // Theme Color Picker
-            if (Current.Services.DataTypeService.GetDataType(VendrCheckoutConstants.DataTypes.Guids.ThemeColorPickerGuid) == null)
+            var currentColorPicker = Current.Services.DataTypeService.GetDataType(VendrCheckoutConstants.DataTypes.Guids.ThemeColorPickerGuid);
+            if (currentColorPicker == null)
             {
                 if (Current.PropertyEditors.TryGet(Constants.PropertyEditors.Aliases.ColorPicker, out IDataEditor editor))
                 {
@@ -35,9 +36,34 @@ namespace Vendr.Checkout.Pipeline.Implement.Tasks
                     Current.Services.DataTypeService.Save(dataType);
                 }
             }
+            else
+            {
+                currentColorPicker.Configuration = new ColorPickerConfiguration
+                {
+                    Items = VendrCheckoutConstants.ColorMap.Select((kvp, idx) => new ValueListConfiguration.ValueListItem
+                    {
+                        Id = idx,
+                        Value = "{\"value\":\"" + kvp.Key + "\", \"label\":\"" + kvp.Value + "\"}"
+                    }).ToList(),
+                    UseLabel = false
+                };
+
+                Current.Services.DataTypeService.Save(currentColorPicker);
+            }
 
             // Step Picker
-            if (Current.Services.DataTypeService.GetDataType(VendrCheckoutConstants.DataTypes.Guids.StepPickerGuid) == null)
+            var stepPickerItems = new List<ValueListConfiguration.ValueListItem>
+            {
+                new ValueListConfiguration.ValueListItem { Id = 1, Value = "Information" },
+                new ValueListConfiguration.ValueListItem { Id = 2, Value = "ShippingMethod" },
+                new ValueListConfiguration.ValueListItem { Id = 3, Value = "PaymentMethod" },
+                new ValueListConfiguration.ValueListItem { Id = 4, Value = "Review" },
+                new ValueListConfiguration.ValueListItem { Id = 5, Value = "Payment" },
+                new ValueListConfiguration.ValueListItem { Id = 6, Value = "Confirmation" }
+            };
+
+            var currentStepPicker = Current.Services.DataTypeService.GetDataType(VendrCheckoutConstants.DataTypes.Guids.StepPickerGuid);
+            if (currentStepPicker == null)
             {
                 if (Current.PropertyEditors.TryGet(Constants.PropertyEditors.Aliases.DropDownListFlexible, out IDataEditor editor))
                 {
@@ -48,21 +74,23 @@ namespace Vendr.Checkout.Pipeline.Implement.Tasks
                         DatabaseType = ValueStorageType.Nvarchar,
                         Configuration = new DropDownFlexibleConfiguration
                         {
-                            Items = new List<ValueListConfiguration.ValueListItem>
-                            {
-                                new ValueListConfiguration.ValueListItem { Id = 1, Value = "Information" },
-                                new ValueListConfiguration.ValueListItem { Id = 2, Value = "ShippingMethod" },
-                                new ValueListConfiguration.ValueListItem { Id = 3, Value = "PaymentMethod" },
-                                new ValueListConfiguration.ValueListItem { Id = 4, Value = "Review" },
-                                new ValueListConfiguration.ValueListItem { Id = 5, Value = "Payment" },
-                                new ValueListConfiguration.ValueListItem { Id = 6, Value = "Confirmation" }
-                            },
+                            Items = stepPickerItems,
                             Multiple = false
                         }
                     };
 
                     Current.Services.DataTypeService.Save(dataType);
                 }
+            }
+            else
+            {
+                currentStepPicker.Configuration = new DropDownFlexibleConfiguration
+                {
+                    Items = stepPickerItems,
+                    Multiple = false
+                };
+
+                Current.Services.DataTypeService.Save(currentStepPicker);
             }
 
             // Continue the pipeline
