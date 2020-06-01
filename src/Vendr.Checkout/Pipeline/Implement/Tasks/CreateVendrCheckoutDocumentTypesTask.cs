fc3be1b4ec5e9d761f7b9078865965c98e8f525b
mattbrailsford <me@mattbrailsford.com>
2020-06-01 15:05:24 +0100

    Added code to add properties to doctypes of they are missing

--- src/Vendr.Checkout/Pipeline/Implement/Tasks/CreateVendrCheckoutDocumentTypesTask.cs
+++ src/Vendr.Checkout/Pipeline/Implement/Tasks/CreateVendrCheckoutDocumentTypesTask.cs
@@ -46,6 +46,21 @@ namespace Vendr.Checkout.Pipeline.Implement.Tasks
             }
 
             // Checkout Step Page
+            var checkoutStepProps = new[]{
+                new PropertyType(textstringDataType.Value) {
+                    Alias = "vendrShortStepName",
+                    Name = "Short Step Name",
+                    Description = "A short name for this step to display in the checkout navigation.",
+                    SortOrder = 10
+                },
+                new PropertyType(stepPickerDataType.Value) {
+                    Alias = "vendrStepType",
+                    Name = "Step Type",
+                    Description = "The checkout step to display for this step of the checkout flow.",
+                    SortOrder = 20
+                }
+            };
+
             existing = Current.Services.ContentTypeService.Get(VendrCheckoutConstants.ContentTypes.Guids.CheckoutStepPageGuid);
             
             if (existing == null)
@@ -57,20 +72,7 @@ namespace Vendr.Checkout.Pipeline.Implement.Tasks
                     Name = "[Vendr Checkout] Checkout Step Page",
                     Icon = "icon-settings-alt color-green",
                     PropertyGroups = new PropertyGroupCollection(new[]{
-                        new PropertyGroup(new PropertyTypeCollection(true, new[]{
-                            new PropertyType(textstringDataType.Value) {
-                                Alias = "vendrShortStepName",
-                                Name = "Short Step Name",
-                                Description = "A short name for this step to display in the checkout navigation.",
-                                SortOrder = 10
-                            },
-                            new PropertyType(stepPickerDataType.Value) {
-                                Alias = "vendrStepType",
-                                Name = "Step Type",
-                                Description = "The checkout step to display for this step of the checkout flow.",
-                                SortOrder = 20
-                            }
-                        })) {
+                        new PropertyGroup(new PropertyTypeCollection(true, checkoutStepProps)) {
                             Name = "Settings",
                             SortOrder =100
                         }
@@ -83,10 +85,97 @@ namespace Vendr.Checkout.Pipeline.Implement.Tasks
             }
             else
             {
+                var safeExisting = false;
+                var hasSettingsGroup = existing.PropertyGroups.Contains("settings");
+                var settingsGroup = hasSettingsGroup
+                    ? existing.PropertyGroups["Settings"]
+                    : new PropertyGroup(new PropertyTypeCollection(true, checkoutStepProps)) 
+                    {
+                        Name = "Settings",
+                        SortOrder =100
+                    };
+
+                foreach (var prop in checkoutStepProps)
+                {
+                    if (!settingsGroup.PropertyTypes.Contains(prop.Alias))
+                    {
+                        settingsGroup.PropertyTypes.Add(prop);
+                        safeExisting = true;
+                    }
+                }
+
+                if (!hasSettingsGroup)
+                {
+                    existing.PropertyGroups.Add(settingsGroup);
+                    safeExisting = true;
+                }
+
+                if (safeExisting)
+                {
+                    Current.Services.ContentTypeService.Save(existing);
+                }
+
                 checkoutStepPageContentTypeId = existing.Id;
             }
 
             // Checkout Page
+            var checkoutPageProps = new[]{
+                new PropertyType(mediaPickerDataType.Value) {
+                    Alias = "vendrStoreLogo",
+                    Name = "Store Logo",
+                    Description = "A logo image for the store to appear at the top of the checkout screens and order emails.",
+                    SortOrder = 10
+                },
+                new PropertyType(textstringDataType.Value) {
+                    Alias = "vendrStoreAddress",
+                    Name = "Store Address",
+                    Description = "The address of the web store to appear in the footer of order emails.",
+                    SortOrder = 20
+                },
+                new PropertyType(themeColorPickerDataType.Value) {
+                    Alias = "vendrThemeColor",
+                    Name = "Theme Color",
+                    Description = "The theme color to use for colored elements of the checkout pages.",
+                    SortOrder = 30
+                },
+                new PropertyType(booleanDataType.Value) {
+                    Alias = "vendrCollectShippingInfo",
+                    Name = "Collect Shipping Info",
+                    Description = "Select whether to collect shipping information or not. Not necessary if you are only dealing with digital downloads.",
+                    SortOrder = 40
+                },
+                new PropertyType(textstringDataType.Value) {
+                    Alias = "vendrOrderLinePropertyAliases",
+                    Name = "Order Line Property Aliases",
+                    Description = "Comma separated list of order line property aliases to display in the order summary.",
+                    SortOrder = 50
+                },
+                new PropertyType(contentPickerDataType.Value) {
+                    Alias = "vendrCheckoutBackPage",
+                    Name = "Checkout Back Page",
+                    Description = "The page to go back to when backing out of the checkout flow.",
+                    SortOrder = 60
+                },
+                new PropertyType(contentPickerDataType.Value) {
+                    Alias = "vendrTermsAndConditionsPage",
+                    Name = "Terms and Conditions Page",
+                    Description = "The page on the site containing the terms and conditions.",
+                    SortOrder = 70
+                },
+                new PropertyType(contentPickerDataType.Value) {
+                    Alias = "vendrPrivacyPolicyPage",
+                    Name = "Privacy Policy Page",
+                    Description = "The page on the site containing the privacy policy.",
+                    SortOrder = 80
+                },
+                new PropertyType(booleanDataType.Value) {
+                    Alias = "umbracoNaviHide",
+                    Name = "Hide from Navigation",
+                    Description = "Hide the checkout page from the sites main navigation.",
+                    SortOrder = 90
+                }
+            };
+
             existing = Current.Services.ContentTypeService.Get(VendrCheckoutConstants.ContentTypes.Guids.CheckoutPageGuid);
 
             if (existing == null)
@@ -101,62 +190,7 @@ namespace Vendr.Checkout.Pipeline.Implement.Tasks
                         new ContentTypeSort(checkoutStepPageContentTypeId, 1)
                     },
                     PropertyGroups = new PropertyGroupCollection(new[]{
-                        new PropertyGroup(new PropertyTypeCollection(true, new[]{
-                            new PropertyType(mediaPickerDataType.Value) {
-                                Alias = "vendrStoreLogo",
-                                Name = "Store Logo",
-                                Description = "A logo image for the store to appear at the top of the checkout screens and order emails.",
-                                SortOrder = 10
-                            },
-                            new PropertyType(textstringDataType.Value) {
-                                Alias = "vendrStoreAddress",
-                                Name = "Store Address",
-                                Description = "The address of the web store to appear in the footer of order emails.",
-                                SortOrder = 20
-                            },
-                            new PropertyType(themeColorPickerDataType.Value) {
-                                Alias = "vendrThemeColor",
-                                Name = "Theme Color",
-                                Description = "The theme color to use for colored elements of the checkout pages.",
-                                SortOrder = 30
-                            },
-                            new PropertyType(booleanDataType.Value) {
-                                Alias = "vendrCollectShippingInfo",
-                                Name = "Collect Shipping Info",
-                                Description = "Select whether to collect shipping information or not. Not necessary if you are only dealing with digital downloads.",
-                                SortOrder = 40
-                            },
-                            new PropertyType(textstringDataType.Value) {
-                                Alias = "vendrOrderLinePropertyAliases",
-                                Name = "Order Line Property Aliases",
-                                Description = "Comma separated list of order line property aliases to display in the order summary.",
-                                SortOrder = 50
-                            },
-                            new PropertyType(contentPickerDataType.Value) {
-                                Alias = "vendrCheckoutBackPage",
-                                Name = "Checkout Back Page",
-                                Description = "The page to go back to when backing out of the checkout flow.",
-                                SortOrder = 60
-                            },
-                            new PropertyType(contentPickerDataType.Value) {
-                                Alias = "vendrTermsAndConditionsPage",
-                                Name = "Terms and Conditions Page",
-                                Description = "The page on the site containing the terms and conditions.",
-                                SortOrder = 70
-                            },
-                            new PropertyType(contentPickerDataType.Value) {
-                                Alias = "vendrPrivacyPolicyPage",
-                                Name = "Privacy Policy Page",
-                                Description = "The page on the site containing the privacy policy.",
-                                SortOrder = 80
-                            },
-                            new PropertyType(booleanDataType.Value) {
-                                Alias = "umbracoNaviHide",
-                                Name = "Hide from Navigation",
-                                Description = "Hide the checkout page from the sites main navigation.",
-                                SortOrder = 90
-                            }
-                        })) {
+                        new PropertyGroup(new PropertyTypeCollection(true, checkoutPageProps)) {
                             Name = "Settings",
                             SortOrder = 50
                         }
@@ -164,6 +198,38 @@ namespace Vendr.Checkout.Pipeline.Implement.Tasks
                 };
 
                 Current.Services.ContentTypeService.Save(contentType);
+            }
+            else
+            {
+                var safeExisting = false;
+                var hasSettingsGroup = existing.PropertyGroups.Contains("settings");
+                var settingsGroup = hasSettingsGroup
+                    ? existing.PropertyGroups["Settings"]
+                    : new PropertyGroup(new PropertyTypeCollection(true, checkoutPageProps))
+                    {
+                        Name = "Settings",
+                        SortOrder = 100
+                    };
+
+                foreach (var prop in checkoutPageProps)
+                {
+                    if (!settingsGroup.PropertyTypes.Contains(prop.Alias))
+                    {
+                        settingsGroup.PropertyTypes.Add(prop);
+                        safeExisting = true;
+                    }
+                }
+
+                if (!hasSettingsGroup)
+                {
+                    existing.PropertyGroups.Add(settingsGroup);
+                    safeExisting = true;
+                }
+
+                if (safeExisting)
+                {
+                    Current.Services.ContentTypeService.Save(existing);
+                }
             }
 
             // Continue the pipeline
