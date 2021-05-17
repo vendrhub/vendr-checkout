6ea71d5263206e74f7e7b6663aa2c20a436c6fe1
mattbrailsford <me@mattbrailsford.com>
2021-05-17 10:40:09 +0100

    Fix bug in empty cart check when not using cookies

---
+++ src/Vendr.Checkout/Web/Controllers/VendrCheckoutBaseController.cs
@@ -0,0 +1,37 @@
+﻿using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+using Vendr.Core.Api;
+using Vendr.Core.Models;
+
+namespace Vendr.Checkout.Web.Controllers
+{
+    public abstract class VendrCheckoutBaseController : RenderMvcController
+    {
+        protected bool IsValidCart(out string redirectUrl)
+        { 
+            var store = CurrentPage.Value<StoreReadOnly>(Core.Constants.Properties.StorePropertyAlias, fallback: Fallback.ToAncestors);
+            var order = !IsConfirmationPageType(CurrentPage)
+                ? VendrApi.Instance.GetCurrentOrder(store.Id)
+                : VendrApi.Instance.GetCurrentFinalizedOrder(store.Id);
+
+            if (order == null || order.OrderLines.Count == 0)
+            {
+                var backPage = CurrentPage.Value<IPublishedContent>("vendrCheckoutBackPage", fallback: Fallback.ToAncestors);
+                redirectUrl = backPage != null ? backPage.Url : "/";
+                return false;
+            }
+
+            redirectUrl = null;
+            return true;
+        }
+
+        private bool IsConfirmationPageType(IPublishedContent node)
+        {
+            if (node == null || node.ContentType == null || !node.HasProperty("vendrStepType"))
+                return false;
+
+            return node.ContentType.Alias == VendrCheckoutConstants.ContentTypes.Aliases.CheckoutStepPage && node.Value<string>("vendrStepType") == "Confirmation";
+        }
+    }
+}
