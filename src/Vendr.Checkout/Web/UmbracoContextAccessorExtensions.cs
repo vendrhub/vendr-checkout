c1afbaa85e55895d31f553bce8f505323163254d
mattbrailsford <me@mattbrailsford.com>
2021-08-24 14:51:45 +0100

    Fix views accessing umbraco content cache

---
+++ src/Vendr.Checkout/Web/UmbracoContextAccessorExtensions.cs
@@ -0,0 +1,15 @@
+﻿#if NETFRAMEWORK
+
+using Umbraco.Web;
+
+namespace Vendr.Checkout.Web
+{
+    public static class UmbracoContextAccessorExtensions
+    {
+        // Extension method to standardize the API between v8 and v9
+        public static UmbracoContext GetRequiredUmbracoContext(this IUmbracoContextAccessor umbracoContextAccessor)
+            => umbracoContextAccessor.UmbracoContext;
+    }
+}
+
+#endif
