7200c618b88f4ae71003bdb8ab613278453528f6
mattbrailsford <me@mattbrailsford.com>
2022-02-24 15:52:29 +0000

    Reduce number of scripts needed

--- src/Vendr.Checkout/Web/UI/App_Plugins/VendrCheckout/scripts/vendrcheckout.js
+++ src/Vendr.Checkout/Web/UI/App_Plugins/VendrCheckout/scripts/vendrcheckout.js
@@ -1,29 +1,22 @@
 ﻿(function () {
 
     // Initialization
-    function initSm() {
+    function init() {
 
-        $("body").on("click.sm", "#order-summary-toggle", function (e) {
+        // Setup order summary toggle
+        $("#order-summary-toggle").on("click", function (e) {
             e.preventDefault();
             var toShow = $("#order-summary").hasClass("hidden");
             if (toShow) {
-                showMobileOrderSummary();
+                $("#order-summary").removeClass("hidden");
+                $("#order-summary-toggle__text-open").removeClass("hidden");
+                $("#order-summary-toggle__text-closed").addClass("hidden");
             } else {
-                hideMobileOrderSummary();
+                $("#order-summary").addClass("hidden");
+                $("#order-summary-toggle__text-open").addClass("hidden");
+                $("#order-summary-toggle__text-closed").removeClass("hidden");
             }
         });
-
-        hideMobileOrderSummary();
-
-    }
-
-    function initLg() {
-
-        $("body").off(".sm");
-
-    }
-
-    function initCommon() {
 
         // Display billing address regions if any
         $("select[name='billingAddress.Country']").on("change", function () {
@@ -37,55 +30,26 @@
 
         // Toggle shipping address display
         $("input[name=shippingSameAsBilling]").on("click", function () {
-            showHideShippingInfo(true);
-        });
-
-        var acceptTerms = $("#acceptTerms");
-        if (acceptTerms.length > 0) {
-            acceptTerms.on("click", enableDisableContinueButton);
-            enableDisableContinueButton();
-        }
-
-        var errors = $(".validation-summary-errors");
-        if (errors.length > 0) {
-            Toastify({
-                text: errors.find("ul li:first-child").text(),
-                duration: 3000,
-                gravity: "bottom", // `top` or `bottom`
-                position: 'center', // `left`, `center` or `right`
-                backgroundColor: "#f56565",
-                className: "",
-                stopOnFocus: true, // Prevents dismissing of toast on hover
-            }).showToast();
-        }
-
-        showHideShippingInfo(false);
-    }
-
-    function showMobileOrderSummary() {
-        $("#order-summary").removeClass("hidden");
-        $("#order-summary-toggle__text-open").removeClass("hidden");
-        $("#order-summary-toggle__text-closed").addClass("hidden");
-    }
-
-    function hideMobileOrderSummary() {
-        $("#order-summary").addClass("hidden");
-        $("#order-summary-toggle__text-open").addClass("hidden");
-        $("#order-summary-toggle__text-closed").removeClass("hidden");
-    }
-
-    function showHideShippingInfo(clearValues) {
-        var hideShippingInfo = $("input[name=shippingSameAsBilling]").is(":checked");
-        if (hideShippingInfo) {
-            $("#shipping-info").hide();
-        } else {
-            if (clearValues) {
-                //$("input[type=text][name^=shipping]").val("");
+            var hideShippingInfo = $(this).is(":checked");
+            if (hideShippingInfo) {
+                $("#shipping-info").addClass("hidden");
+            } else {
+                $("#shipping-info").removeClass("hidden");
             }
-            $("#shipping-info").show();
-        }
+        }).triggerHandler("click");
+
+        // Enable / disable continue button when accepting terms
+        $("#acceptTerms").on("click", function () {
+            var enableContinueButton = $("#acceptTerms").is(":checked");
+            if (enableContinueButton) {
+                $("#continue").attr("disabled", false);
+            } else {
+                $("#continue").attr("disabled", true);
+            }
+        }).triggerHandler("click");
     }
 
+    // Helper functions
     function showRegions(addressType, regions) {
 
         var sl = $("select[name='" + addressType + "Address.Region']");
@@ -109,46 +73,15 @@
         }
 
         if (regions.length > 0) {
-            sl.removeClass("hidden")
-                .addClass("block");
+            sl.removeClass("hidden").addClass("block");
         } else {
-            sl.removeClass("block")
-                .addClass("hidden");
+            sl.removeClass("block").addClass("hidden");
         }
 
     };
 
-    function enableDisableContinueButton() {
-
-        var enableContinueButton = $("#acceptTerms").is(":checked");
-        if (enableContinueButton) {
-            $("#continue").attr("disabled", false)
-                .css("backgroundColor", "")
-                .css("color", "")
-                .css("cursor", "");
-        } else {
-            $("#continue").attr("disabled", true)
-                .css("backgroundColor", "#f7fafc")
-                .css("color", "#ddd")
-                .css("cursor", "no-drop");
-        }
-
-    }
-
-    // Setup responsive states
-    ssm.addState({
-        id: 'sm',
-        query: '(max-width: 1023px)',
-        onEnter: initSm
-    });
-
-    ssm.addState({
-        id: 'lg',
-        query: '(min-width: 1024px)',
-        onEnter: initLg
-    });
-
-    initCommon();
+    // Trigger init
+    init();
 
 })();
 
