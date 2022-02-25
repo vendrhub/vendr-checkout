dbec6cbeabbf660f6e0315bdabff42d586f9f47e
mattbrailsford <me@mattbrailsford.com>
2022-02-25 10:58:15 +0000

    Minify css for production

--- src/Vendr.Checkout.Client/postcss.config.js
+++ src/Vendr.Checkout.Client/postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     plugins: [
         require('postcss-import'),
-        require('tailwindcss'),
+        require('tailwindcss')
     ]
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins.push(require('cssnano'))
 }
