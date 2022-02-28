0aba38ecee2c2f6e0ab8d14c997fd5daecbd0740
mattbrailsford <me@mattbrailsford.com>
2022-02-28 14:56:00 +0000

    - Disaply "Select" option in country / region dropdowns - Apply required validation on shipping address when different to billing - Disable required shipping fields when shipping is same as billing - Use disabled CSS utilities to hide region dropdowns when it's disabled

--- src/Vendr.Checkout/Web/UI/App_Plugins/VendrCheckout/scripts/vendrcheckout.js
+++ src/Vendr.Checkout/Web/UI/App_Plugins/VendrCheckout/scripts/vendrcheckout.js
@@ -15,21 +15,21 @@
 
         // Display billing address regions if any
         document.querySelectorAll("select[name='billingAddress.Country']").forEach(el => {
-            var h = () => showRegions("billing", JSON.parse(el.selectedOptions[0].dataset.regions));
+            var h = () => { toggleShippingRequiredInputValidation(); showRegions("billing", JSON.parse(el.selectedOptions[0].dataset.regions)); }
             el.addEventListener("change", h);
             h();
         });
 
         // Display shipping address regions if any
         document.querySelectorAll("select[name='shippingAddress.Country']").forEach(el => {
-            var h = () => showRegions("shipping", JSON.parse(el.selectedOptions[0].dataset.regions));
+            var h = () => { toggleShippingRequiredInputValidation(); showRegions("shipping", JSON.parse(el.selectedOptions[0].dataset.regions)); }
             el.addEventListener("change", h);
             h();
         });
 
         // Toggle shipping address display
         document.querySelectorAll("input[name=shippingSameAsBilling]").forEach(el => {
-            var h = () => document.getElementById("shipping-info").classList.toggle("hidden", el.checked);
+            var h = () => { document.getElementById("shipping-info").classList.toggle("hidden", el.checked); toggleShippingRequiredInputValidation(); }
             el.addEventListener("change", h);
             h();
         });
@@ -44,17 +44,30 @@
     }
 
     // Helper functions
-    function showRegions(addressType, regions)
-    {
+    function toggleShippingRequiredInputValidation() {
+        var shippingSameAsBillingEl = document.querySelector("input[name=shippingSameAsBilling]");
+        var shippingSameAsBilling = shippingSameAsBillingEl && shippingSameAsBillingEl.checked;
+        document.querySelectorAll("#shipping-info [required]").forEach(el => {
+            el.disabled = shippingSameAsBilling; // Disable any shipping required fields (this overrides the required validation)
+        });
+    }
+
+    function showRegions(addressType, regions) {
         var sl = document.querySelector("select[name='" + addressType + "Address.Region']");
         var slVal = sl.dataset.value;
 
         sl.innerHTML = "";
 
         var hasRegions = regions.length > 0;
-        if (hasRegions)
-        {
+        if (hasRegions) {
             var containsValue = false;
+
+            var opt = document.createElement('option');
+            opt.value = "";
+            opt.text = sl.dataset.placeholder;
+            opt.disabled = true;
+            opt.selected = true;
+            sl.appendChild(opt);
 
             regions.forEach(function (itm, idx) {
                 var opt = document.createElement('option');
@@ -70,8 +83,8 @@
             }
         }
 
-        sl.classList.toggle("hidden", !hasRegions);
-        sl.classList.toggle("block", hasRegions);
+        sl.required = hasRegions;
+        sl.disabled = !hasRegions;
     };
 
     // Trigger init
